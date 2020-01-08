509dc7adb3263c20d2d574fe34ae975b1f33b247
galy <galy@student.42.fr>
2020-01-08 11:52:26 +0100

    add chat routes

--- api/index.js
+++ api/index.js
@@ -3,10 +3,12 @@ const healthTest = require('./health/health.js');
 const googleApi = require('./googleApi/routes');
 const events = require('./events/routes');
 const users = require('./users/routes');
+const chats = require('./chats/routes');
 
 router.get('/health', healthTest.heathyResponse);
 router.use('/external', googleApi);
 router.use('/events', events);
 router.use('/users', users);
+router.use('/chats', chats);
 
 module.exports = router;
