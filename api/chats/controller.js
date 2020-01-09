70b75911fb7b28738e4f8c5d6a47836f73be8254
galy <galy@student.42.fr>
2020-01-09 10:00:45 +0100

    chat controller save new messages ok

--- api/chats/controller.js
+++ api/chats/controller.js
@@ -10,37 +10,40 @@ module.exports = {
 
   saveNewMessage: async (req, res, next) => {
     try {
-    console.clear();
-    console.log('saveNewMessage');
-    const { chatId } = req.params;
-    const [payload] = req.body;
+      console.clear();
+      console.log('saveNewMessage');
+      const { chatId } = req.params;
+      const [payload] = req.body;
+      
+      payload.user._id = req.user.id;
 
-    payload.user._id = req.user.id;
-    console.log('Chat id:', chatId);
-    console.log('Message payload', payload);
-
-    const filePath = `${chatDir}/room_${chatId}.log`;
-
-    fs.openSync(filePath, 'a+');
-    
-    // open, write, close the data to the log file
-      const rawData = fs.readFileSync(filePath);
-      console.log('0001', rawData.length);
-      let data = {};
+      const filePath = `${chatDir}/room_${chatId}_2.log`;
+      // * open log file in read/write mode
+      const fd = fs.openSync(filePath, 'w+');
+      
+      // * get the content of the log file
+      const rawData = fs.readFileSync(fd);
+      // console.log('0001', rawData.length);
+      let data = {
+        messages: [],
+      };
       if (rawData.length > 0) {
         data = JSON.parse(rawData);
-        console.log('0002a', data);
-      } else {
-        console.log('0002b', data);
       }
-      // console.log('0001', rawData)
+
+      // * add the new message to the log file content
+      data.messages.push(payload);
+      // * write the new data object to the log file
+      fs.writeFileSync(fd, JSON.stringify(data));
+      // * close the file descriptor
+      fs.closeSync(fd);
 
       return res.status(200).send();
     } catch (error) {
       console.error(error);
       return res.status(500).send();
     }
-
   },
+
 
 };
