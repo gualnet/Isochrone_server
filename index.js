d0a73fad222f2582f826f72cd29b54e59ef495c0
galy <galy@student.42.fr>
2020-01-09 10:50:04 +0100

    socket implementation

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const Middleware = require('./middleware');
 const morgan = require('morgan');
+// const http = require('http');
 const DB = require('./database/index');
 const dbSync = require('./database/syncDB');
 
 // Init express
 const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: false, limit: '50mb' }));
@@ -13,6 +17,15 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 // CORS
 app.use(Middleware.cors)
+
+// Socket
+io.on("connection", socket => {
+  console.log("a user connected :D");
+  socket.on("chat message", msg => {
+    console.log(msg);
+    io.emit("chat message", msg);
+  });
+});
 
 // Routing
 app.use('/api', require('./api/index'));
@@ -24,4 +37,4 @@ app.use((req, res, next) => res.status(404).send('404 - Not Found'))
 // Just to know it
 console.log('\x1b[36m%s\x1b[0m', '✓ App is now fully running', );
 
-module.exports = app;
+module.exports = { app: app, server: server };
